9d97965bd72ebfee60906fcfe8d9e8859dc1b813
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-01-30 14:15:38 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <s-toast />
+</template>
+
+<script>
+
+import SToast from "@/components/toast/SToast.vue";
+
+export default {
+  name: 'App',
+  components: {
+    SToast
+  }
+}
+</script>
---
+++ src/components/toast/SToast.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="toast">
+    <div class="toast-icon">icon</div>
+    <div class="toast-content">
+      <div class="toast-title">Success</div>
+      <div class="toast-message">Data has been saved.</div>
+    </div>
+    <button class="toast-button">&times;</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SToast'
+}
+</script>
